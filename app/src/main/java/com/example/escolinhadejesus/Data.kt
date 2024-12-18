b1cbab8e3b4e405046194e64f3c9c15efc00f58b
IsaacYusuke <129394732+IsaacYusuke@users.noreply.github.com>
2024-12-17 21:01:52 -0300

    Paralitico do tanque + quebra de linha nos textos biblicos

--- app/src/main/java/com/example/escolinhadejesus/Data.kt
+++ app/src/main/java/com/example/escolinhadejesus/Data.kt
@@ -14,7 +14,8 @@ val graficos = listOf(
         texto = """
             Davi e Golias
             
-            Texto Bíblico: 1 Samuel 17
+            Texto Bíblico: 
+            1 Samuel 17
             
             Golias era um gigante muito forte, mas Davi tinha a arma mais poderosa de todas: a confiança em Deus!
             Toque no Davi para ver como ele venceu Golias
@@ -26,7 +27,8 @@ val graficos = listOf(
         texto = """
             Jesus Acalma a Tempestade
             
-            Texto Bíblico: Marcos 4:35-41
+            Texto Bíblico: 
+            Marcos 4:35-41
             
             Os discípulos estavam com muito medo por causa da tempestade no mar, mas então eles lembraram que Jesus estava ali no barco dormindo!
             Toque no mar para ver o que aconteceu quando Jesus disse "Silêncio!"            
@@ -38,7 +40,8 @@ val graficos = listOf(
         texto = """
             O Sonho de Jacó
             
-            Texto Bíblico: Gênesis 28
+            Texto Bíblico: 
+            Gênesis 28
             
             Quando Jacó saiu de casa fugindo de seu irmão, ele dormiu e teve um sonho. Nesse sonho, ele viu uma escada que ia até o Céu, e os anjos subiam e desciam por ela.
             Essa escada representa Jesus, que faz a ponte entre nós e Deus!
@@ -50,7 +53,8 @@ val graficos = listOf(
         texto = """
             Os 3 Amigos na Fornalha
             
-            Texto Bíblico: Daniel 3
+            Texto Bíblico: 
+            Daniel 3
             
             Sadraque, Mesaque e Abede-Nego foram lançados na fornalha porque não adoraram a estátua de ouro do rei. Porém, quando o rei olhou lá dentro, ele teve uma surpresa!
             Toque na fornalha para ver o que aconteceu.
@@ -62,11 +66,25 @@ val graficos = listOf(
         texto = """
             A Mulher de Ló
             
-            Texto Bíblico: Gênesis 19
+            Texto Bíblico: 
+            Gênesis 19
             
             Deus destruiu as cidades de Sodoma e Gomorra porque as pessoas de lá eram más. Ló e sua família deveriam fugir sem olhar para trás, mas a mulher de Ló olhou!
             Toque na família de Ló e acompanhe eles para ver o que aconteceu.
         """.trimIndent(),
         arquivoJson = "AMulherDeLo.json"
+    ),
+    GraficoData(
+        titulo = "O Paralítico do Tanque",
+        texto = """
+            O Paralítico do Tanque
+            
+            Texto Bíblico: 
+            João 5
+            
+            Muitos doentes ficavam ao redor do tanque de Betesda porque achavam que aquela água poderia curá-los. Um homem paralítico estava ali havia 38 anos, esperando por uma cura.
+            Toque no paralítico para ver o que aconteceu quando Jesus falou com ele!
+        """.trimIndent(),
+        arquivoJson = "OParaliticoDoTanque.json"
     )
 )
