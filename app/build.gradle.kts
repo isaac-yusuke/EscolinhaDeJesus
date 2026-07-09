9e7056f41fc4ca4ae1c701e30a35fb57666429cc
IsaacYusuke <isaacyusuke@gmail.com>
2026-07-08 21:59:14 -0300

    Arco íris

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.isaacyyanagui.escolinhadejesus"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7     //Tem que mudar sempre que fizer nova versao
-        versionName = "3.0" //Tem que mudar sempre que fizer nova versao
+        versionCode = 8     //Tem que mudar sempre que fizer nova versao
+        versionName = "3.1" //Tem que mudar sempre que fizer nova versao
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
